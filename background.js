7dd959e784359e341b0539e42ced9d61f6041186
Dongzy <dzy201415@gmail.com>
2017-03-29 14:09:40 +0800

    close the tab automatically

--- background.js
+++ background.js
@@ -1,9 +1,22 @@
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
-		if (request.storage == "all")
+		if (request.command == "storage")
 			sendResponse({username: localStorage.getItem('username'), 
 						password: localStorage.getItem('password'), 
 						auto: localStorage.getItem('autologin')});
+		else if(request.command == "close")
+			chrome.tabs.getSelected(function(tab) {
+				chrome.tabs.getAllInWindow(function(tab2) {
+					if(tab2.length > 1) {
+						chrome.tabs.remove(tab.id);
+					}
+					else {
+						chrome.tabs.update(tab.id, {
+							url: "https://www.baidu.com/"
+						});
+					}
+				});
+			});
 	}
 );
 if(localStorage.length!=3){
--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-chrome.runtime.sendMessage({storage: "all"}, function(response) {
+chrome.runtime.sendMessage({command: "storage"}, function(response) {
 	if(response.auto=='true'){
 		if(document.URL.indexOf("https://cas.sustc.edu.cn/cas/login") >= 0 )   
 		{
@@ -17,7 +17,7 @@ chrome.runtime.sendMessage({storage: "all"}, function(response) {
 		} else if(document.URL.indexOf("http://enet.10000.gd.cn:10001/sz/sz112/transfer.jsp") >= 0 ) {
 			document.getElementsByTagName('input')[0].click();
 		} else if(document.URL.indexOf("http://enet.10000.gd.cn:10001/sz/sz112/index.jsp") >=0 ){
-			window.location.href="https://www.baidu.com/";
+			chrome.runtime.sendMessage({command: "close"});
 		}
 	}
 });
