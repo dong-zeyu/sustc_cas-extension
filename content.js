97a094d7ca7d0935fe1f2dba36536d30bf6db875
Dongzy <dzy201415@gmail.com>
2017-12-14 17:44:19 +0800

    fix the error info display

--- content.js
+++ content.js
@@ -2,18 +2,14 @@ chrome.runtime.sendMessage({command: "storage"}, function(response) {
 	if(response.auto=='true'){
 		if(document.URL.indexOf("https://cas.sustc.edu.cn/cas/login") >= 0 )   
 		{
-			if(document.getElementById('msg')==null){
-					console.log('login...');
-					document.getElementById('username').value = response.username;
-					document.getElementById('password').value = response.password;
-					document.getElementsByName('submit')[0].click();
-
+			if(document.getElementById('fm1').firstElementChild.tagName!="DIV"){
+				console.log('login...');
+				document.getElementById('username').value = response.username;
+				document.getElementById('password').value = response.password;
+				document.getElementsByName('submit')[0].click();
 			}
 			else{
-				if(document.getElementById('msg').getAttribute('class')=='errors'&&
-				document.getElementById('msg').innerText=="认证信息无效。"){
-					document.getElementById('msg').innerText = '用户名密码错误！请在插件中更新信息并从地址栏刷新页面';
-				}
+				document.getElementById('fm1').firstElementChild.innerText += "    请从插件中更新用户名和密码并刷新页面"
 			}
 		} else if(document.URL.indexOf("http://enet.10000.gd.cn:10001/sz/sz112/transfer.jsp") >= 0 ) {
 			document.getElementsByTagName('input')[0].click();
