606d146cb3bed3cbbded36e57ef3e13ae224c704
Dongzy <dzy201415@gmail.com>
2019-03-22 16:42:24 +0800

    add new domain name

--- content.js
+++ content.js
@@ -2,15 +2,20 @@ chrome.runtime.sendMessage({
     command: "storage"
 }, function(response) {
     if (response.auto == 'true') {
-        if (document.URL.indexOf("https://cas.sustc.edu.cn/cas/login") >= 0 && document.getElementById('fm1') != null) {
-            if (document.getElementById('fm1').firstElementChild.tagName != "DIV") {
-                console.log('login...');
-                document.getElementById('username').value = response.username;
-                document.getElementById('password').value = response.password;
-                document.getElementsByName('submit')[0].removeAttribute('disabled')
-                document.getElementsByName('submit')[0].click();
-            } else {
-                document.getElementById('fm1').firstElementChild.innerText += "    请从插件中更新用户名和密码并从地址栏刷新页面"
+        if (document.URL.indexOf("https://cas.sustc.edu.cn/cas/login") >= 0 || document.URL.indexOf("https://cas.sustech.edu.cn/cas/login") >= 0) {
+            if (document.getElementById('fm1') != null) {
+                if (document.getElementById('fm1').firstElementChild.tagName != "DIV") {
+                    console.log('login...');
+                    document.getElementById('username').value = response.username;
+                    document.getElementById('password').value = response.password;
+                    document.getElementsByName('submit')[0].removeAttribute('disabled')
+                    document.getElementsByName('submit')[0].click();
+                } else {
+                    document.getElementById('fm1').firstElementChild.innerText += "    请从插件中更新用户名和密码并从地址栏刷新页面"
+                }
+            }
+            else {
+                console.error("Failed to locate login form!");
             }
         } else if (document.URL.indexOf("http://enet.10000.gd.cn:10001/sz/sz112/transfer.jsp") >= 0) {
             document.getElementsByTagName('input')[0].click();
